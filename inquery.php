981d9d760eb0e04018b4ee51a3ad1470193e96f7
s19008 <s19008@std.it-college.ac.jp>
2021-05-12 10:18:38 +0900

    Practice

---
+++ inquery.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: お問い合わせ
+*/
+?>
+<table class="inquery">
+    <tr>
+        <th>お名前</th>
+        <td>[mwform_text name="お名前" size="60" placeholder="お名前を入力してください"]</td>
+    </tr>
+    <tr>
+        <th>よみがな</th>
+        <td></td>
+    </tr>
+    <tr>
+        <th>住所</th>
+        <td></td>
+    </tr>
+    <tr>
+        <th>電話番号</th>
+        <td></td>
+    </tr>
+    <tr>
+        <th>メールアドレス</th>
+        <td></td>
+    </tr>
+    <tr>
+        <th>メールアドレス(確認)</th>
+        <td></td>
+    </tr>
+    <tr>
+        <th>お問い合わせ</th>
+        <td></td>
+    </tr>
+</table><!-- /.inquery -->
