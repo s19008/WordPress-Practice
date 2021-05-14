ea88fb06e170d851f92d688bfb1802dea3b1789b
s19008 <s19008@std.it-college.ac.jp>
2021-05-15 00:29:09 +0900

    Practice

--- inquery.php
+++ inquery.php
@@ -3,33 +3,15 @@
 Template Name: お問い合わせ
 */
 ?>
-<table class="inquery">
-    <tr>
-        <th>お名前</th>
-        <td>[mwform_text name="お名前" size="60" placeholder="お名前を入力してください"]</td>
-    </tr>
-    <tr>
-        <th>よみがな</th>
-        <td></td>
-    </tr>
-    <tr>
-        <th>住所</th>
-        <td></td>
-    </tr>
-    <tr>
-        <th>電話番号</th>
-        <td></td>
-    </tr>
-    <tr>
-        <th>メールアドレス</th>
-        <td></td>
-    </tr>
-    <tr>
-        <th>メールアドレス(確認)</th>
-        <td></td>
-    </tr>
-    <tr>
-        <th>お問い合わせ</th>
-        <td></td>
-    </tr>
-</table><!-- /.inquery -->
+
+<?php get_header(); ?>
+
+<div class="space">
+<?php while ( have_posts() ) { ?>
+    <h1><?php the_title(); ?></h1>
+    <?php the_post(); ?>
+    <?php the_content(); ?>
+<?php } ?>
+</div>
+
+<?php get_footer(); ?>
