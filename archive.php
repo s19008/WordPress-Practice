bb5c7ee2b7d8d98c6ee7964cc6d28c73ece329ad
s19008 <s19008@std.it-college.ac.jp>
2021-05-18 23:58:24 +0900

    Practice

---
+++ archive.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div class="blog-top">
+</div><!-- /.blog-top -->
+<div class="breadcrumbs" typeof="BreadcrumbList" vocab="https://schema.org/">
+    <?php if(function_exists('bcn_display'))
+    {
+        bcn_display();
+    }?>
+</div>
+<div class="blog-title">
+    <h2>ブログ</h2>    
+    <p>Blog</p>
+</div><!-- /.blog-title -->
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -24,10 +24,11 @@ function my_script_init() {
     wp_enqueue_style( 'slick', get_template_directory_uri() . '/slick/slick.css', array(), '1.0.0', 'all');
     wp_enqueue_style( 'slick-theme', get_template_directory_uri() . '/slick/slick-theme.css', array(), '1.0.0', 'all');
     wp_enqueue_style( 'aos-css', '//unpkg.com/aos@2.3.1/dist/aos.css', array() );
-    wp_enqueue_style( 'main', get_template_directory_uri() . '/style.css', array( 'reset', 'slick', 'slick-theme', 'aos-css'), '1.0.0', 'all');
-    wp_enqueue_script( 'slick-js', get_template_directory_uri() . '/slick/slick.min.js', array(), '1.0.0', true );
+    wp_enqueue_style( 'main', get_template_directory_uri() . '/style.css', array( 'reset', 'slick', 'slick-theme', 'aos-css'), 'all');
+    wp_enqueue_script( 'slick-js', get_template_directory_uri() . '/slick/slick.min.js', array(), true );
     wp_enqueue_script( 'aos-js', '//unpkg.com/aos@2.3.1/dist/aos.js', array(), true );
-    wp_enqueue_script( 'my-jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js', array(), true );
-    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/javascript/script.js', array( 'slick-js', 'my-jquery' ), '1.0.0', true );
+    wp_enqueue_script( 'my-jquery', '//ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js', array( 'slick-js','aos-js' ), true );
+    wp_enqueue_script( 'main-js', get_template_directory_uri() . '/javascript/script.js', array( 'my-jquery' ), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'my_script_init' );
+?>
