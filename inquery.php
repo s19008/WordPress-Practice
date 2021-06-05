942ccc2aae74bc290fcb5bb5bf83e891b6af863b
s19008 <s19008@std.it-college.ac.jp>
2021-06-06 00:18:09 +0900

    Practice

--- inquery.php
+++ inquery.php
@@ -6,17 +6,19 @@ Template Name: お問い合わせ
 
 <?php get_header(); ?>
 
-<?php 
+<div class="inquery-top">
+</div><!-- /.inquery-top -->
+<?php
 breadcrumb($postID);
 ?>
 <div class="space">
     <?php if (have_posts()) :
         while (have_posts()) : ?>
             <h1><?php the_title(); ?></h1>
+            <p>Contact</p>
     <?php
             the_post();
             the_content();
-
         endwhile;
     else :
     endif;
