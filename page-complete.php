cc083be5b6f1bd9b8a66bfa823d7fd9f604fa408
s19008 <s19008@std.it-college.ac.jp>
2021-06-08 00:05:07 +0900

    Practice

---
+++ page-complete.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<div class="complete-top">
+</div><!-- /.complete-top -->
+
+<?php
+breadcrumb($postID);
+?>
+
+<div class="complete-inner">
+    <h2 class="complete-text">お問い合わせいただきありがとうございます。 <br>内容を確認した後に担当者よりご連絡を差し上げます。</h2><!-- /.inner-text -->
+    <div class="back-btn"><a href="/">ホームへ戻る</a></div><!-- /.back-btn -->
+</div><!-- /.complete-inner -->
+
+
+<?php get_footer(); ?>
